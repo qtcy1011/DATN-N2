f75c5718daefc086b81dbca5e75b3986ff846a47
qtcy1011 <qtcy1011@gmail.com>
2025-10-14 18:21:11 +0700

    Initial Spring Boot commit

---
+++ src/main/java/com/poly/da/DatnN2Application.java
@@ -0,0 +1,13 @@
+package com.poly.da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnN2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnN2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/da/DatnN2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnN2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
